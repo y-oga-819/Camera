e2bfed63775d4c5c20c47209aac81a53a9a67074
yuki819 <yuki819@mba2018.local>
2022-07-15 01:46:08 +0900

    init(Camera): init project

---
+++ Shared/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Shared
+//
+//  Created by 小笠原佑樹 on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
